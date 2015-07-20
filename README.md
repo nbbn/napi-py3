35027a63900e2e38e132aaa1b5044741fa54f9a6
Jakub Stepniak <jakub.st@gmail.com>
2015-07-21 01:40:34 +0200

    readme and python3 support/utf-8 as default output

---
+++ README.md
@@ -0,0 +1,14 @@
+# napi-py3
+
+It's rewritten napi.py to Python 3. All subtitles are converted to utf-8 encoding.
+7z required.
+
+I've found on the Internet original script years ago.
+It's almost impossible to point original source, feel free to google fragments of original file (the first commit) to find potential source.
+
+## Usage
+
+```sh
+chmod +x napi.py
+./napi.py filename_of_movie
+```
--- napi.py
+++ napi.py
@@ -1,19 +1,8 @@
-#!/usr/bin/python
-# reversed napi 0.16.3.1
-#
-# by gim,krzynio,dosiu,hash 2oo8.
-#
-# 
-#
-# last modified: 6-I-2oo8
-#
-# 4pc0h f0rc3
-#
-# do dzialania potrzebny jest p7zip-full (tak sie nazywa paczka w debianie)
-#
-# POZDRAWIAMY NASZYCH FANOW!
+#!/usr/bin/env python3
+# original file by gim,krzynio,dosiu,hash 2oo8.
+# modified by github.com/nbbn
 
-import md5, sys, urllib, os
+import hashlib, sys, urllib.request, os
 
 
 def f(z):
@@ -22,7 +11,7 @@ def f(z):
     add = [0, 0xd, 0x10, 0xb, 0x5]
 
     b = []
-    for i in xrange(len(idx)):
+    for i in range(len(idx)):
         a = add[i]
         m = mul[i]
         i = idx[i]
@@ -33,27 +22,26 @@ def f(z):
 
     return ''.join(b)
 
-
 if (len(sys.argv) == 1):
-    print
-    "wy*dalaj na stadion po film"
+    print("wy*dalaj na stadion po film")
     sys.exit(2)
 
-d = md5.new();
-d.update(open(sys.argv[1]).read(10485760))
+d = hashlib.md5();
+d.update(open(sys.argv[1], mode='br').read(10485760))
 
 str = "http://napiprojekt.pl/unit_napisy/dl.php?l=PL&f=" + d.hexdigest() + "&t=" + f(
     d.hexdigest()) + "&v=other&kolejka=false&nick=&pass=&napios=" + os.name
 
-open("napisy.7z", "w").write(urllib.urlopen(str).read())
+open("napisy.7z", "bw").write(urllib.request.urlopen(str).read())
 nazwa = sys.argv[1][:-3] + 'txt'
 
 if (os.system("/usr/bin/7z x -y -so -piBlm8NTigvru0Jr0 napisy.7z 2>/dev/null >\"" + nazwa + "\"")):
-    print
-    "nie ma napisa do filmu"
+    print("nie ma napisa do filmu")
     os.remove(nazwa)
 else:
-    print
-    "napisy pobrano, milordzie!"
-
+    print("napisy pobrano, milordzie!")
 os.remove("napisy.7z")
+
+lines = [line for line in open(nazwa, 'r', encoding='cp1250')]
+os.remove(nazwa)
+open(nazwa, 'w').writelines(lines)
